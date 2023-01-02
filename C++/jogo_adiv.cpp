22dc1b1e93dcb080329feb389668241193328577
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-01-02 20:38:15 -0300

    Add files via upload

--- C++/jogo_adiv.cpp
+++ C++/jogo_adiv.cpp
@@ -6,8 +6,26 @@ int main(){
     
     cout << "---- Bem vindo ao jogo ----" << endl;
 
-    int numero_sec = 42;
+    int numero_sec = 27;
 
-    cout << "O numero secreto eh 42" << endl;
+    cout << "O numero secreto eh "<< numero_sec << "!" << endl;
     
+    int chute;
+
+    cout << "Digite o valor do seu chute: " << endl;
+    cin >> chute;
+    cout << "O chute foi " << chute << endl;
+
+    if ( numero_sec == chute){
+        cout << "Vc acertou o numero secreto! " << endl;
+    }
+    else{
+        if (chute > numero_sec){
+            cout << "O seu chute foi MAIOR que o numero gerado!"<< endl;
+        
+        }
+        else{
+            cout << " O chute foi MENOR que o numero gerado!" << endl;
+        }
+    }
 }
