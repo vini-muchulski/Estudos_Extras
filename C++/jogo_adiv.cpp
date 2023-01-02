87e6a6d81fa969dab799df3d8013b6b1d2617657
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-01-02 19:20:58 -0300

    Add files via upload

---
+++ C++/jogo_adiv.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    cout << "---- Bem vindo ao jogo ----" << endl;
+
+    int numero_sec = 42;
+
+    cout << "O numero secreto eh 42" << endl;
+    
+}
