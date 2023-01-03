d6eec50628a18bc7a1acea766602b1ae3eb52352
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-01-03 19:58:46 -0300

    Add files via upload

--- C++/jogo_forca.cpp
+++ C++/jogo_forca.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include<map>
 #include<vector>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
 
 using namespace std;
 
-const string palavra_sec = "ABACAXI";
+string palavra_sec = "ABACAXI";
 
 map<char,bool> ja_chutou;
 vector<char> chutes_errados;
@@ -29,6 +33,63 @@ bool contem_letra(char chute){
 }
 
 
+bool nao_acertou(){
+    for(char letra : palavra_sec){
+        // se a letra n foi chutada, ao menos uma letra falta para a palavra estar completa, entao algum valor 
+        // no map vai estar falso ainda
+        if(!ja_chutou[letra]){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool nao_enforcou(){
+    if(chutes_errados.size() == 5){
+        return false;
+    }
+    return true;
+
+    //return chutes_errados.size() < 5;
+}
+
+vector<string> le_arquivo(){
+    ifstream arquivo;
+    string palavra_lida;
+    arquivo.open("palavras.txt");
+
+    vector<string> palavras_do_arquivo;
+
+    int qnt_palavras;
+    arquivo >> qnt_palavras;
+
+    cout << " O ARQUIVO POSSUI " << qnt_palavras << endl;
+
+       for(int i=0;i<qnt_palavras;i++){
+
+        arquivo >> palavra_lida;
+        palavras_do_arquivo.push_back(palavra_lida);
+
+        cout << "LINHA " << i << " palavra " << palavra_lida << endl;
+        }
+
+        return palavras_do_arquivo;
+
+
+}
+
+
+void sortear_palavra(){
+    vector<string> palavras = le_arquivo();
+
+    int tam = palavras.size();
+
+    srand(time(NULL));
+    const int numero = rand() % tam;
+
+    palavra_sec = palavras[numero];
+}
+    
 
 
 int main(){
@@ -39,11 +100,12 @@ int main(){
     cout << "**** Jogo da forca ****" << endl;
     cout << " "<< endl;
 
-    bool nao_acertou = true;
-    bool nao_enforcou = true;
+    
+    sortear_palavra();
+
     char chute;
 
-    while (nao_acertou && nao_enforcou)
+    while (nao_acertou() && nao_enforcou())
     {
         for(char letra: palavra_sec){
             if(ja_chutou[letra] ){
@@ -76,6 +138,19 @@ int main(){
             cout << letra << " ";
         }
         cout << " " << endl;
+        
     }
+
+        cout << " " << endl;
+        cout << " FIM DE JOGO " << endl;
+        cout << " A palavra secreta era: "<< palavra_sec << endl;
+        cout << " " << endl;
+
+        if(nao_acertou()){
+            cout << " PERDEU! Voce foi enforcado! " << endl;
+        }
+        else{
+            cout << " GANHOU! Voce foi ACERTOU a palavra! " << endl;
+        }
     
 }
