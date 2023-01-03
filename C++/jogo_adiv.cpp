a8058217cf3cb293ba842f980bf051b61c242d7b
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-01-02 21:36:07 -0300

    Add files via upload

--- C++/jogo_adiv.cpp
+++ C++/jogo_adiv.cpp
@@ -3,29 +3,57 @@ using namespace std;
 
 
 int main(){
-    
-    cout << "---- Bem vindo ao jogo ----" << endl;
+    cout << "****************************" << endl;
+    cout << "----- Bem vindo ao jogo ----" << endl;
+    cout << "****************************" << endl;
 
-    int numero_sec = 27;
+    const int numero_sec = 27;
+    int tentativas = 0;
 
-    cout << "O numero secreto eh "<< numero_sec << "!" << endl;
-    
+    //cout << "O numero secreto eh "<< numero_sec << "!" << endl;
     int chute;
-
+    cout << "Tentativa " << tentativas << endl;
     cout << "Digite o valor do seu chute: " << endl;
     cin >> chute;
     cout << "O chute foi " << chute << endl;
 
-    if ( numero_sec == chute){
-        cout << "Vc acertou o numero secreto! " << endl;
-    }
-    else{
-        if (chute > numero_sec){
-            cout << "O seu chute foi MAIOR que o numero gerado!"<< endl;
+    bool nao_acertou = chute != numero_sec;
+
+    bool acertou = numero_sec == chute;
+    if (acertou){
+            cout << "Voce acertou o numero secreto! " << endl;
+        }
+
+
+    while(nao_acertou){
+
+        acertou = numero_sec == chute;
+        tentativas++;
         
+
+        if (acertou){
+            cout << "Voce acertou o numero secreto em "<< tentativas <<" tentativas!" << endl;
+            break;
         }
         else{
-            cout << " O chute foi MENOR que o numero gerado!" << endl;
+            if (chute > numero_sec){
+                cout << "O seu chute foi MAIOR que o numero gerado!"<< endl;
+            
+            }
+            else{
+                cout << "O chute foi MENOR que o numero gerado!" << endl;
+            }
         }
-    }
+
+        cout << "Digite outro chute: " << endl;
+        cin >> chute;
+        cout << "O chute foi " << chute << endl;
+        cout << "Tentativa " << tentativas << endl;
+            
+        }
+
+        
+    
+
+    
 }
