fa6e6a7b7a58deb6bb40b5fa097f93d94c255e24
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-01-03 18:19:50 -0300

    Add files via upload

--- C++/jogo_forca.cpp
+++ C++/jogo_forca.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <string>
+#include<map>
+#include<vector>
+
 using namespace std;
 
 const string palavra_sec = "ABACAXI";
 
-bool contem_letra(char letra){
+map<char,bool> ja_chutou;
+vector<char> chutes_errados;
+
+bool contem_letra(char chute){
     int i;
  
+    /*
     for (i=0; i < palavra_sec.size() ; i++){
-        if(letra == palavra_sec[i]){
+        if(chute == palavra_sec[i]){
             return true;
         }
     }
+    */
+
+    for(char letra : palavra_sec)
+        if(chute == letra){
+            return true;
+        }
     return false;
 }
 
 
+
+
 int main(){
     
-    cout << palavra_sec << endl;
+    //cout << palavra_sec << endl;
+
+    cout << " "<< endl;
+    cout << "**** Jogo da forca ****" << endl;
+    cout << " "<< endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
@@ -26,16 +45,37 @@ int main(){
 
     while (nao_acertou && nao_enforcou)
     {
+        for(char letra: palavra_sec){
+            if(ja_chutou[letra] ){
+                cout << letra <<" ";
+            }
+            else{
+                cout << "_ ";
+            }
+        }
+
+        cout << " ";
         cin >> chute;
+        ja_chutou[chute] = true;
+
         cout<< " O CHUTE FOI = " << chute << endl;
 
         if(contem_letra(chute)){
             cout  << "Voce acertou o chute! Esta na palavra" << endl;
+            cout << " "<< endl;
         
          }
         else{
             cout  << "Voce ERROU o chute" << endl;
+            cout << " "<< endl;
+            chutes_errados.push_back(chute);
         }
+
+            cout << "Chutes errados: ";
+            for(char letra : chutes_errados){
+            cout << letra << " ";
+        }
+        cout << " " << endl;
     }
     
 }
