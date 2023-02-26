1d4b85acf11edae263a344a735923d53914bfe0f
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-02-26 15:23:55 -0300

    Add files via upload

--- Front/JS/C2/main.js
+++ Front/JS/C2/main.js
@@ -4,10 +4,31 @@ const somar = document.querySelector("#somar")
 const subtrair = document.querySelector("#subtrair")
 const braco = document.querySelector("#braco")
 
-robo.addEventListener("click", olaMundo )
 
-function olaMundo(nome){
-    console.log("clique"+ nome);
+const controle = document.querySelectorAll(".controle-ajuste")
+console.log(controle)
+
+somar.addEventListener("click", function(){
+    gerenciaValor("somar")
+} )
+
+subtrair.addEventListener("click", function(){
+    gerenciaValor("subtrair")
+} )
+
+function gerenciaValor(operacao){
+    if(operacao === "somar"){
+        braco.value  = parseInt(braco.value ) + 1
+    }
+    else{
+        braco.value  = parseInt(braco.value ) - 1
+    
+    }
+    
 }
 
-olaMundo("Vini")
+controle.forEach( function(elemento,indice){
+    console.log(indice)
+    console.log(elemento)
+})
+
