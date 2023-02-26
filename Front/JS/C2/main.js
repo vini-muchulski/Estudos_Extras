f2343b8b0b9623b7e26693ada92630bd59e138fd
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-02-26 16:11:03 -0300

    Add files via upload

--- Front/JS/C2/main.js
+++ Front/JS/C2/main.js
@@ -1,34 +1,29 @@
-//alert('oi')
-
-const somar = document.querySelector("#somar")
-const subtrair = document.querySelector("#subtrair")
-const braco = document.querySelector("#braco")
-
 
 const controle = document.querySelectorAll(".controle-ajuste")
-console.log(controle)
 
-somar.addEventListener("click", function(){
-    gerenciaValor("somar")
-} )
 
-subtrair.addEventListener("click", function(){
-    gerenciaValor("subtrair")
-} )
+function gerenciaValor(operacao, controle){
+    const peca = controle.querySelector(".controle-contador")
 
-function gerenciaValor(operacao){
-    if(operacao === "somar"){
-        braco.value  = parseInt(braco.value ) + 1
+    if(operacao === "+"){
+        peca.value  = parseInt(peca.value ) + 1
     }
-    else{
-        braco.value  = parseInt(braco.value ) - 1
+    else if (operacao === "-"){
+        peca.value  = parseInt(peca.value ) - 1
     
     }
     
 }
 
 controle.forEach( function(elemento,indice){
-    console.log(indice)
-    console.log(elemento)
+    elemento.addEventListener("click", function (evento){
+
+        const sinal = evento.target.textContent
+        const controle = evento.target.parentNode
+        console.log(controle)
+
+        gerenciaValor(sinal,controle)
+    } )
 })
+
 
