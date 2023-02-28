8e3a181fc05257012dd4a5febf4c8f1647dffcdd
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-02-27 22:25:11 -0300

    Add files via upload

--- Front/JS/C3/main.js
+++ Front/JS/C3/main.js
@@ -1,15 +1,36 @@
-const delta = document.querySelector("#novoItem");
+//projeto da mochila
 
-console.log(delta);
+const delta = document.getElementById("novoItem");
+const lista = document.getElementById("lista")
+
+//console.log(delta);
 
 delta.addEventListener("submit", function (evento){
     evento.preventDefault()
 
-    const nomeItem = evento.target[0].value
-    const quantidade = evento.target[1].value
+    const nomeItem = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
+    
 
-
-    console.log("OK!!")
-    console.log(quantidade);
-    console.log(nomeItem);
+    geraElemento(nomeItem,quantidade)
 })
+
+function geraElemento(nome,quantidade){
+
+    
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem) 
+    novoItem.innerHTML += nome
+
+    
+
+    lista.appendChild(novoItem)
+
+
+}
