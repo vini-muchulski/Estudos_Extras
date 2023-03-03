176213aec9a815c42201e11c155783e5f166343f
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-03-03 14:27:30 -0300

    Add files via upload

--- Front/JS/C5/script.js
+++ Front/JS/C5/script.js
@@ -1,5 +1,14 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
-.then(r =>console.log(r));
+.then(r =>{
+    if (r.erro){
+        throw Error('CEP nao existe')
+    }
+    else
+        console.log(r)
+    })
+
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento finalizado'))
 
 console.log(consultaCEP)
