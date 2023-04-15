028c8a5105d4678a3084114cdf20b6b9071add08
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-04-15 19:09:10 -0300

    Add files via upload

--- C++/free_code_camp/oop.cpp
+++ C++/free_code_camp/oop.cpp
@@ -10,7 +10,7 @@ using namespace std;
     class Funcionario{
 
 // atributos
-public:
+private:
     string Nome;
     string Empresa;
     int Idade;
@@ -19,9 +19,6 @@ public:
 // metodos
 public:
 
-void se_apresente(){
-    cout << "Funcionario \nNome: " << Nome <<" - Empresa: "<< Empresa <<  " - Idade: " << Idade << endl;;
-}
 
 Funcionario(string nome, string empresa, int idade){
     Nome = nome;
@@ -30,25 +27,60 @@ Funcionario(string nome, string empresa, int idade){
 }
 
 
+void se_apresente(){
+    cout << "Funcionario \nNome: " << Nome <<" - Empresa: "<< Empresa <<  " - Idade: " << Idade << endl;;
+}
+
+
+
+void setName( string name){
+    Nome = name;
+}
+
+void setEmpresa( string empresa){
+    Empresa = empresa;
+}
+
+void setIdade( int idade){
+    if (idade <18){
+        cout << "So sao permitidos funcionarios com mais de 18 anos de idade. " << endl;
+    }else{
+    Idade = idade;
+    }
+}
+
+
+
+string getName(){
+    return Nome;
+}
+
+string getEmpresa(){
+    return Empresa;
+}
+
+int getIdade(){
+    return Idade;
+}
+
+
 };
 
 int main(){
 
     Funcionario func1 = Funcionario("Vini","SpaceX",25);
-    
 
     func1.se_apresente();
 
 
     Funcionario func2 = Funcionario("Apeiron","NASA",32);
-   
 
     func2.se_apresente();
+
+    cout << func1.getName() << " tem " << func1.getIdade() << " anos " << endl;
+
+    func2.setIdade(14);
+    cout << func2.getIdade() << endl;
     
-
-
-
-
-
 return 0;
 }
